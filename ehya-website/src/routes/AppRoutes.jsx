0ee9dba84624a32fc739d397f9f9ef6fd7c72afb
VSah7079 <vk3815155@gmail.com>
2025-09-20 11:44:11 +0530

    refactor: Simplify routing structure in AppRoutes component

--- ehya-website/src/routes/AppRoutes.jsx
+++ ehya-website/src/routes/AppRoutes.jsx
@@ -1,10 +1,10 @@
+
 import Home from "../pages/Home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Features from "../pages/Landing";
-
 import HashtagPerformance from "../pages/HashtagPerformance ";
 import Docs from "../pages/Docs";
 import Help from "../pages/Help";
@@ -14,21 +14,13 @@ export default function AppRoutes() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-1">
-        <section id="home">
-          <Home />
-        </section>
-        <section id="features">
-          <Features />
-        </section>
-        <section id="hashtags">
-          <HashtagPerformance />
-        </section>
-        <section id="docs">
-          <Docs />
-        </section>
-        <section id="help">
-          <Help />
-        </section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/hashtags" element={<HashtagPerformance />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
       </div>
       <Footer />
     </div>
